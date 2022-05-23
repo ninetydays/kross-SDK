72db55ea5a01486c1f637fd8b2411e7395cd263e
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2022-05-23 14:58:59 +0500

    fix: revert the recent fix and add the new one which works for broswer

--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,76 @@ const path = require('path')
 const package = require('./package.json')
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(m?js|ts|tsx|jsx|js)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: {
-                                        browsers: '> 1%, IE 11, not dead',
-                                    },
-                                    modules: false, // Needed for tree shaking to work.
-                                },
-                            ],
-                        ],
-                    },
+  module: {
+    rules: [
+      {
+        test: /\.(m?js|ts|tsx|jsx|js)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    browsers: '> 1%, IE 11, not dead',
+                  },
+                  modules: false, // Needed for tree shaking to work.
                 },
-            },
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-        ],
-    },
-    target: 'web', // or web
-    entry: {
-        'kross-SDK': path.resolve(__dirname, 'src/index.ts'),
-    },
-    resolve: {
-        mainFiles: ['index'],
-        extensions: ['.ts', '.js', '.tsx', '.jsx'],
-    },
-
-    output: {
-        filename: `index.js`,
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
-        globalObject: 'this',
-    },
-
-    mode: 'production',
-
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendors: {
-                    priority: -10,
-                    test: /[\\/]node_modules[\\/]/,
-                    reuseExistingChunk: true,
-                },
-            },
-            filename: `index.min.js`,
-            chunks: 'async',
-            minChunks: 1,
-            minSize: 30000,
+              ],
+            ],
+          },
         },
+      },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+  target: 'web', // or web
+  entry: {
+    'kross-SDK': path.resolve(__dirname, 'src/index.ts'),
+  },
+  resolve: {
+    mainFiles: ['index'],
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
     },
-    stats: {
-        colors: true,
+  },
+
+  output: {
+    filename: `index.js`,
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: 'this',
+  },
+
+  mode: 'production',
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+          reuseExistingChunk: true,
+        },
+      },
+      filename: `index.min.js`,
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
     },
-    devtool: 'source-map',
+  },
+  stats: {
+    colors: true,
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  devtool: 'source-map',
 }
