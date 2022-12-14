c5f7503106163bed0ffccd45301d3314b6ca4bd3
Aziyat Murzamidinov <15@kross.kr>
2022-12-14 14:58:05 +0900

    chore: initial function for api call

--- src/services/api/note/getNotes.ts
+++ src/services/api/note/getNotes.ts
@@ -1,28 +1,28 @@
-import oliveClient from '../../oliveClient';
-import { AxiosRequestConfig } from 'axios';
+import { KrossClient } from 'kross-sdk';
 
-  type GetNotesResponse = {
-    user_id: number;
-    state: any,
-    limit: any,
-    offset: any,
-    sortBy: any,
-  };
+type GetNotesResponse = {
+  user_id: any; 
+  state: any,
+  limit: any,
+  offset: any,
+  sortBy: any,
+};
 
-export const getNotes = async (data : GetNotesResponse) => {
-    const config: AxiosRequestConfig = {
-        method: 'get',
-        url:`/user/${data.user_id}/notes`,
-        params: {
-            ...data
-        }
-      };
-    
-    try {
-      const response = await oliveClient(config);
-        console.log(response);
-        return response;
-    } catch (error) {
-      console.error(error);
-    }
-  }
+export const getNoteList = async (data : GetNotesResponse) => {
+    const orderBy = data.state === 'done' ? 'doneAt' : 'issueAt';
+    const params = {
+        orderBy,
+        ...data
+    };
+
+    const client = new KrossClient({
+        baseURL: process.env.REACT_APP_OLIVE_BASE_URL,
+        accessId: data.user_id,
+        secretKey: 'secretkey'
+    })
+
+    const resp = await client.get(`/user/${data.user_id}/notes`, {
+        params,
+    });
+    return resp.data;
+}
--- src/services/oliveClient.ts
+++
@@ -1,5 +0,0 @@
-import { AxiosPromise, AxiosRequestConfig } from 'axios';
-import apiClient from './apiClient';
-
-export default (config: AxiosRequestConfig): AxiosPromise =>
-  apiClient({ ...config, baseURL: process.env.REACT_APP_OLIVE_BASE_URL, });
