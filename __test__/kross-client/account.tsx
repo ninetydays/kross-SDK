f9abec31767641ed439627f9b818553d6e500f8a
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2023-01-24 22:33:25 +0500

    chore: added fixes for interceptor and updates test cases

--- __test__/kross-client/account.tsx
+++ __test__/kross-client/account.tsx
@@ -4,91 +4,91 @@ import React from 'react';
 import { act, renderHook, waitFor } from '@testing-library/react';
 
 export const account = () => {
-    let client: Account;
-    const baseURL = 'https://olive-dev.kross.kr';
-    const accessId = 'XLD7UY9GETOK7TPY';
-    const secretKey = 'yLbVRHGgwT5c22ndOVT2';
-    const queryClient = new QueryClient({
-      defaultOptions: {
-        queries: {
-          // ✅ turns retries off
-          retry: false,
-        },
+  let client: Account;
+  const baseURL = 'https://olive-dev.kross.kr';
+  const accessId = 'XLD7UY9GETOK7TPY';
+  const secretKey = 'yLbVRHGgwT5c22ndOVT2';
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // ✅ turns retries off
+        retry: false,
       },
+    },
+  });
+  const wrapper = ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+
+  beforeAll(() => {
+    client = new Account({
+      baseURL,
+      accessId,
+      secretKey,
+      adapter: require('axios/lib/adapters/http'),
     });
-    const wrapper = ({ children }) => (
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    );
-  
-    beforeAll(() => {
-      client = new Account({
-        baseURL,
-        accessId,
-        secretKey,
-        adapter: require('axios/lib/adapters/http'),
+  });
+
+  it('checks bank account owner request', async () => {
+    const { check } = client.useAccountHooks();
+    const { result } = renderHook(() => check(), {
+      wrapper,
+    });
+    await act(async () => {
+      await result.current.mutateAsync({
+        bankId: '049',
+        accountNumber: 'random',
+        name: 'randomName',
       });
+    });
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(result.current.data).toBeDefined();
+  }, 10000);
+
+  it('checks request to withdraw amount via withdrawInit', async () => {
+    const { withdrawInit } = client.useAccountHooks();
+    const { result } = renderHook(() => withdrawInit(), {
+      wrapper,
     });
 
-    it('checks bank account owner request', async () => {
-      const { check } = client.useAccountHooks();
-      const { result } = renderHook(() => check(), {
-        wrapper,
+    await act(async () => {
+      await result.current.mutateAsync({
+        member_no: 0,
+        amount: 0,
       });
-      await act(async () => {
-        await result.current.mutateAsync({
-          bankId: '049',
-          accountNumber: 'random',
-          name: 'randomName',
-        });
+    });
+    await waitFor(() => expect(result.current?.isSuccess).toBe(true));
+
+    expect(result.current.data).toBeDefined();
+  }, 10000);
+
+  it('checks request to cancel withdraw amount via withdrawCancel', async () => {
+    const { withdrawCancel } = client.useAccountHooks();
+    const { result } = renderHook(() => withdrawCancel(), {
+      wrapper,
+    });
+    await act(async () => {
+      await result.current.mutateAsync({
+        idempotency_key: '0',
       });
-      await waitFor(() => expect(result.current.isSuccess).toBe(true));
-  
-      expect(result.current.data).toBeDefined();
-    }, 10000);
-  
-    it('checks request to withdraw amount via withdrawInit', async () => {
-      const { withdrawInit } = client.useAccountHooks();
-      const { result } = renderHook(() => withdrawInit(), {
-        wrapper,
+    });
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    expect(result.current.data).toBeDefined();
+  }, 30000);
+
+  it('registers withdraw account via withdrawVerify', async () => {
+    const { withdrawVerify } = client.useAccountHooks();
+    const { result } = renderHook(() => withdrawVerify(), {
+      wrapper,
+    });
+    await act(async () => {
+      await result.current.mutateAsync({
+        idempotency_key: '0',
+        verify_code: '1234',
       });
-  
-      await act(async () => {
-        await result.current.mutateAsync({
-          member_no: 0,
-          amount: 0,
-        });
-      });
-      await waitFor(() => expect(result.current?.isSuccess).toBe(true));
-  
-      expect(result.current.data).toBeDefined();
-    }, 10000);
-  
-    it('checks request to cancel withdraw amount via withdrawCancel', async () => {
-      const { withdrawCancel } = client.useAccountHooks();
-      const { result } = renderHook(() => withdrawCancel(), {
-        wrapper,
-      });
-      await act(async () => {
-        await result.current.mutateAsync({
-          idempotency_key: '0',
-        });
-      });
-      await waitFor(() => expect(result.current.isSuccess).toBe(true));
-      expect(result.current.data).toBeDefined();
-    }, 30000);
-  
-    it('registers withdraw account via withdrawVerify', async () => {
-      const { withdrawVerify } = client.useAccountHooks();
-      const { result } = renderHook(() => withdrawVerify(), {
-        wrapper,
-      });
-      await act(async () => {
-        await result.current.mutateAsync({
-          idempotency_key: '0',
-          verify_code: '0',
-        });
-      });
-      await waitFor(() => expect(result.current.isSuccess).toBe(true));
-      expect(result.current.data).toBeDefined();
-    }, 30000);
-}
+    });
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    expect(result.current.data).toBeDefined();
+  }, 30000);
+};
--- __test__/kross-client/base.tsx
+++ __test__/kross-client/base.tsx
@@ -4,54 +4,53 @@ import React from 'react';
 import { act, renderHook, waitFor } from '@testing-library/react';
 
 export const base = () => {
-    let client: KrossClient;
-    const baseURL = 'https://olive-dev.kross.kr';
-    const accessId = 'XLD7UY9GETOK7TPY';
-    const secretKey = 'yLbVRHGgwT5c22ndOVT2';
-    const queryClient = new QueryClient({
-      defaultOptions: {
-        queries: {
-          // ✅ turns retries off
-          retry: false,
-        },
+  let client: KrossClient;
+  const baseURL = 'https://olive-dev.kross.kr';
+  const accessId = 'XLD7UY9GETOK7TPY';
+  const secretKey = 'yLbVRHGgwT5c22ndOVT2';
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // ✅ turns retries off
+        retry: false,
       },
+    },
+  });
+  const wrapper = ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+  beforeAll(() => {
+    client = new KrossClient({
+      baseURL,
+      accessId,
+      secretKey,
+      adapter: require('axios/lib/adapters/http'),
     });
-    const wrapper = ({ children }) => (
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    );
-    beforeAll(() => {
-      client = new KrossClient({
-        baseURL,
-        accessId,
-        secretKey,
-        adapter: require('axios/lib/adapters/http'),
+  });
+
+  it('gets authToken and refreshToken', async () => {
+    const { useLogin } = client.useAuthHooks();
+    const { result } = renderHook(() => useLogin(), {
+      wrapper,
+    });
+    await act(async () => {
+      await result.current.mutateAsync({
+        keyid: 'mad@kross.kr',
+        password: 'Kross123!',
       });
     });
-  
-    it('gets authToken and refreshToken', async () => {
-      const { useLogin } = client.useAuthHooks();
-      const { result } = renderHook(() => useLogin(), {
-        wrapper,
-      });
-      await act(async () => {
-        await result.current.mutateAsync({
-          keyid: 'mad@kross.kr',
-          password: 'Kross123!',
-        });
-      });
-      await waitFor(() => expect(result.current.isSuccess).toBe(true));
-      expect(result.current.data).toBeDefined();
-    }, 30000);
-  
-    it.skip('updates authToken with refreshToken', async () => {
-      const { updateAuthToken } = client.useAuthHooks();
-      const { result } = renderHook(() => updateAuthToken(), {
-        wrapper,
-      });
-      await act(async () => {
-        await result.current.refetch();
-      });
-      await waitFor(() => expect(result.current.isSuccess).toBe(true));
-      expect(result.current.data).toBeDefined();
-    }, 30000);  
-}
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    expect(result.current.data).toBeDefined();
+  }, 30000);
+
+  it.skip('updates authToken with refreshToken', async () => {
+    const { updateAuthToken } = client.useAuthHooks();
+    const { result } = renderHook(() => updateAuthToken(), {
+      wrapper,
+    });
+    await waitFor(() => expect(result.current.isSuccess).toBe(true), {
+      timeout: 30000,
+    });
+    expect(result.current.data).toBeDefined();
+  }, 30000);
+};
