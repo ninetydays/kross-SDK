dd71aa55107d7a68115209231192083e2bbfa4e8
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2023-02-08 15:16:18 +0500

    chore: added authToken, refreshToken and refreshTokenCalback and refresh handling in  sdk

--- src/types/kross-client/index.ts
+++ src/types/kross-client/index.ts
@@ -6,15 +6,14 @@ export * from './user';
 export * from './investments';
 export * from './account';
 
-type RefreshTokenCallback = (
-  config: AxiosRequestConfig,
-  hmacToken: { hmacToken: string; xDate: string }
-) => Promise<AxiosRequestConfig | undefined>;
+type RefreshTokenCallback = (token: string) => void;
 
 export type KrossClientOptions = AxiosRequestConfig & {
   baseURL: string;
   accessId: string;
   secretKey: string;
+  authToken?: string;
+  refreshToken?: string;
   refreshTokenCallback?: RefreshTokenCallback;
 };
 
