2cd847b9f9d8ca2df0b66423c71ce74922dccf71
jscripter-pk <islam.bscs3275@iiu.edu.pk>
2023-02-13 14:26:46 +0500

    refactor: refactored the webpack config code

--- webpack.config.js
+++ webpack.config.js
@@ -1,163 +1,96 @@
 const path = require('path');
 const package = require('./package.json');
 
-const webConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.(m?js|ts|tsx|jsx|js)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    browsers: '> 1%, IE 11, not dead',
+const returnConfigBaseOnEnv = (env) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(m?js|ts|tsx|jsx|js)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets:
+                      env === 'web'
+                        ? {
+                            browsers: '> 1%, IE 11, not dead',
+                          }
+                        : {
+                            node: 'current',
+                          },
+                    modules: false, // Needed for tree shaking to work.
                   },
-                  modules: false, // Needed for tree shaking to work.
-                },
+                ],
               ],
-            ],
+            },
           },
         },
-      },
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-    ],
-  },
-  target: 'web', // or web
-  entry: {
-    'kross-SDK': path.resolve(__dirname, 'src/index.ts'),
-  },
-  resolve: {
-    mainFiles: ['index'],
-    extensions: ['.ts', '.js', '.tsx', '.jsx'],
-    fallback: {
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
+        { test: /\.tsx?$/, loader: 'ts-loader' },
+      ],
     },
-  },
-
-  output: {
-    filename: `index.js`,
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'commonjs2',
-    globalObject: 'this',
-  },
-
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-    'react-native': 'react-native',
-    'react-query': 'react-query',
-    'date-fns': 'date-fns',
-  },
-
-  mode: 'production',
-
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          priority: -10,
-          test: /[\\/]node_modules[\\/]/,
-          reuseExistingChunk: true,
-        },
-      },
-      filename: `index.min.js`,
-      chunks: 'async',
-      minChunks: 1,
-      minSize: 30000,
+    target: env,
+    entry: {
+      'kross-SDK': path.resolve(__dirname, 'src/index.ts'),
     },
-  },
-  stats: {
-    colors: true,
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
-  devtool: 'source-map',
-};
+    resolve: {
+      mainFiles: ['index'],
+      extensions: ['.ts', '.js', '.tsx', '.jsx'],
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+      },
+    },
 
-const nodeConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.(m?js|ts|tsx|jsx|js)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    node: 'current',
-                  },
-                  modules: false, // Needed for tree shaking to work.
-                },
-              ],
-            ],
+    output: {
+      filename: env === 'web' ? `index.js` : `index.node.js`,
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'commonjs2',
+      globalObject: 'this',
+    },
+
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      'react-native': 'react-native',
+      'react-query': 'react-query',
+      'date-fns': 'date-fns',
+    },
+
+    mode: 'production',
+
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            priority: -10,
+            test: /[\\/]node_modules[\\/]/,
+            reuseExistingChunk: true,
           },
         },
+        filename: `index.min.js`,
+        chunks: 'async',
+        minChunks: 1,
+        minSize: 30000,
       },
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-    ],
-  },
-  target: 'node',
-  entry: {
-    'kross-SDK': path.resolve(__dirname, 'src/index.ts'),
-  },
-  resolve: {
-    mainFiles: ['index'],
-    extensions: ['.ts', '.js', '.tsx', '.jsx'],
-  },
-
-  output: {
-    filename: `index.node.js`,
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'commonjs2',
-    globalObject: 'this',
-  },
-
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-    'react-native': 'react-native',
-    'react-query': 'react-query',
-    'date-fns': 'date-fns',
-  },
-
-  mode: 'production',
-
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          priority: -10,
-          test: /[\\/]node_modules[\\/]/,
-          reuseExistingChunk: true,
-        },
-      },
-      filename: `index.min.js`,
-      chunks: 'async',
-      minChunks: 1,
-      minSize: 30000,
     },
-  },
-  stats: {
-    colors: true,
-  },
-  performance: {
-    hints: false,
-    maxAssetSize: 512000,
-  },
-  devtool: 'source-map',
+    stats: {
+      colors: true,
+    },
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    },
+    devtool: 'source-map',
+  };
 };
+
+const webConfig = returnConfigBaseOnEnv('web');
+const nodeConfig = returnConfigBaseOnEnv('node');
 
 module.exports = [webConfig, nodeConfig];
