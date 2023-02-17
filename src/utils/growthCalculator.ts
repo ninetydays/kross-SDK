44161c6612bc65182f6bd96c3ef7a8cb34a00f2b
Aziyat Murzamidinov <15@kross.kr>
2023-02-17 15:05:37 +0900

    chore: fix lint error

--- src/utils/growthCalculator.ts
+++ src/utils/growthCalculator.ts
@@ -1,6 +1,6 @@
 export const growthCalculator = (totalAssetsArray: any) => {
-  let currentAsset = totalAssetsArray[0][1].totalAssets;
-  let initialAsset = totalAssetsArray[totalAssetsArray.length -1][1].totalAssets;
-  let rate = (currentAsset - initialAsset)/initialAsset * 100;
+  const currentAsset = totalAssetsArray[0][1].totalAssets;
+  const initialAsset = totalAssetsArray[totalAssetsArray.length -1][1].totalAssets;
+  const rate = (currentAsset - initialAsset)/initialAsset * 100;
   return rate;
 }
