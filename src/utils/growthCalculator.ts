e0c21617e0e37be75e2f75fdfd113819ffe89498
Aziyat Murzamidinov <15@kross.kr>
2023-02-17 18:01:42 +0900

    chore: fix NaN when there is no data

--- src/utils/growthCalculator.ts
+++ src/utils/growthCalculator.ts
@@ -1,4 +1,7 @@
 export const growthCalculator = (totalAssetsArray: any) => {
+  if (totalAssetsArray.length === 0){
+    return 0;
+  }
   const currentAsset = totalAssetsArray[0][1].totalAssets;
   const initialAsset =
     totalAssetsArray[totalAssetsArray.length - 1][1].totalAssets;
