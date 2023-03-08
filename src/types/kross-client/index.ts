f300c54243c6b09555f52ca1e709166d08fc6a04
Aziyat Murzamidinov <15@kross.kr>
2023-03-08 15:52:03 +0900

    Revert "remove isBusiness"
    This reverts commit 59931e3c9eb9da5ee4e4b6b4f51dd1ba83c4364e.

--- src/types/kross-client/index.ts
+++ src/types/kross-client/index.ts
@@ -53,6 +53,7 @@ export type UserRegisterDto = {
   keyid: string;
   password: string;
   password2: string;
+  isBusiness?: boolean;
   isBorrower?: boolean;
   email?: string;
   corpRegNo?: string;
